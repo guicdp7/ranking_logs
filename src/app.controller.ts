96ec3a9bba50ecdc1abb612b951abcdf9c06a154
Guilherme Cristino <guilherme.cristino@bitfy.app>
2024-04-17 11:08:28 -0300

    feat(app): receber o upload do arquivo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,34 @@
-import { Controller, Get, Render } from '@nestjs/common';
-
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Redirect,
+  Render,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express/multer';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
   @Render('index')
   index() {
     return {};
+  }
+
+  @Get('import')
+  @Render('import')
+  import() {
+    return {};
+  }
+
+  @Post('upload')
+  @Redirect('/')
+  @UseInterceptors(FileInterceptor('file'))
+  upload(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
   }
 }
